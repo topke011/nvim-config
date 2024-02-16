1887849ed4b0625ea49ccdab07d8c4843837d476
Milan Topalovic <topke@Mac.local>
2024-02-16 19:43:26 +0100

    Updating dependencies and fixing telescope invocation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nvim-config
 This repository contains nvim config. Tested on macos only.
+
+# Dependencies
+- java17
+- fzf
+- fd
--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -44,7 +44,7 @@ end
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', function() require('telescopepickers').prettyFilesPicker({ pickers = 'oldfiles' }) end, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>?', function() require('telescopepickers').prettyFilesPicker({ picker = 'oldfiles' }) end, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
